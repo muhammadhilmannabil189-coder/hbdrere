c042d88e9fa0de7324c03db2104a671064102ba7
Abill <muhammadhilmannabil189@gmail.com>
2026-01-17 17:02:28 +0700

    add photo slider

--- script.js
+++ script.js
@@ -25,4 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
     body.classList.remove("play-music");
   });
 });
+let index = 0;
 
+function showSlide() {
+  const slides = document.querySelector(".slides");
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+  const total = document.querySelectorAll(".slides img").length;
+  index = (index + 1) % total;
+  showSlide();
+}
+
+function prevSlide() {
+  const total = document.querySelectorAll(".slides img").length;
+  index = (index - 1 + total) % total;
+  showSlide();
+}
