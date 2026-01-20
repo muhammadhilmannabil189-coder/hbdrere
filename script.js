832dfbffde8b7116ee49e6576ecfb8793a0c6892
Abill <muhammadhilmannabil189@gmail.com>
2026-01-20 21:15:21 +0700

    make music baackground

--- script.js
+++ script.js
@@ -4,25 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (!audio) return;
 
-  audio.addEventListener("play", () => {
+ document.addEventListener("DOMContentLoaded", () => {
+  const music = document.getElementById("bg-music");
+  const body = document.body;
+
+  if (!music) return;
+
+
+  music.play().then(() => {
     body.classList.add("play-music");
+  }).catch(() => {
 
-  
-    for (let i = 0; i < 25; i++) {
-      const confetti = document.createElement("div");
-      confetti.className = "confetti";
-      confetti.style.left = Math.random() * 100 + "vw";
-      confetti.style.background =
-        ["#ff5fa2", "#6c63ff", "#ffd166"][Math.floor(Math.random() * 3)];
-      confetti.style.animationDuration = 3 + Math.random() * 3 + "s";
-      document.body.appendChild(confetti);
-
-      setTimeout(() => confetti.remove(), 6000);
-    }
-  });
-
-  audio.addEventListener("pause", () => {
-    body.classList.remove("play-music");
+    document.addEventListener("click", () => {
+      music.play();
+      body.classList.add("play-music");
+    }, { once: true });
   });
 });
 
